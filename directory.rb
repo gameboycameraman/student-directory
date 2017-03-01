9e36cc60bf353224b58824d8341d777657059eb4
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-01 13:00:34 +0000

    Adding Input

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleonputs", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -20,13 +20,14 @@ end
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  end  
 end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
