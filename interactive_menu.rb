9e16ea0fdbcda480fa76dbb3efee5339a9022f2b
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-03 11:50:26 +0000

    Interactive Menu

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -1,23 +1,61 @@
+@students =[]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+  name = gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+end
+
 def interactive_menu
-  students =[]
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again:"
-    end
   end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
 interactive_menu
