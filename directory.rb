2dc055fc4e9b450f6ff035216b3b9679349f9766
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-04 14:16:19 +0000

    Saving a file

--- directory.rb
+++ directory.rb
@@ -1,78 +1,74 @@
+@students =[]
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.capitalize.gsub("\n", "")
-
-  puts "And where was this student born?"
-  country_of_birth = gets.capitalize.gsub("\n", "")
-  if country_of_birth.empty?
-    country_of_birth = "Somewhere"
-  end
-  puts "Does he/she has any hobbies?"
-  hobbies = gets.capitalize.gsub("\n", "")
-  if hobbies.empty?
-    hobbies = "No"
-  end
-
-  puts "Which cohort is he/she going to attend?"
-  cohort = gets.capitalize.gsub("\n", "")
-  until ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"].include? cohort
-    puts "We need you to give us a month."
-    cohort = gets.capitalize.gsub("\n", "")
-  end
-
-
+  puts "To finish just hit return twice"
+  name = gets.chomp
   while !name.empty? do
-    students << {name: name, country: country_of_birth, hobbies: hobbies, cohort: cohort}
-    if students.count < 2
-      puts "Now we have 1 student."
-    else
-      puts "Now we have #{students.count} students"
-    end
-    puts "What's the name of the next student? (if none, just press return)"
-    name = gets.capitalize.gsub("\n", "")
-    break if name.empty?
-    puts "And where was this student born?"
-    country_of_birth = gets.capitalize.gsub("\n", "")
-    if country_of_birth.empty?
-      country_of_birth = "Somewhere"
-    end
-    puts "Does he/she has any hobbies?"
-    hobbies = gets.capitalize.gsub("\n", "")
-    if hobbies.empty?
-      hobbies = "No"
-    end
-    puts "Which cohort is he/she going to attend?"
-    cohort = gets.capitalize.gsub("\n", "")
-    until ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"].include? cohort
-      puts "We need you to give us a month."
-      cohort = gets.capitalize.gsub("\n", "")
-    end
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
   end
-  students.sort_by{|student| student[:cohort]}
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again:"
+  end
+end
 
 def print_header
-  puts "The students of Villains Academy".center(100)
-  puts "----------------".center(100)
+  puts "The students of Villains Academy"
+  puts "--------------"
 end
 
-def print(students)
-  number_of_students = 1
-  until number_of_students > students.length
-    puts "#{number_of_students}. #{students[number_of_students -1][:name]} from #{students[number_of_students -1][:country]} - Hobby : #{students[number_of_students -1][:hobbies]} (#{students[number_of_students -1][:cohort]} cohort)".center(100)
-    number_of_students += 1
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students.".center(100)
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
