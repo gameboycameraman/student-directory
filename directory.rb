19fb1181dec04182b59fc75aee5b8a1881a943a2
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-04 16:19:47 +0000

    Taking Arguments from Command Line - Loading List

--- directory.rb
+++ directory.rb
@@ -1,4 +1,18 @@
+ARGV.first
+
 @students =[]
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
 
 def save_students
   file = File.open("students.csv", "w")
@@ -10,21 +24,30 @@ def save_students
   file.close
 end
 
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -32,13 +55,14 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list of students.csv"
   puts "9. Exit"
 end
 
 def show_students
   print_header
   print_students_list
-  print_footer(@students)
+  print_footer
 end
 
 def process(selection)
@@ -49,6 +73,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -71,4 +97,5 @@ def print_footer
   puts "Overall, we have #{@students.count} great students."
 end
 
+try_load_students
 interactive_menu
